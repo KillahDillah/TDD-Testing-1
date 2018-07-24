cec2436c7374a283dd4cb6f41dd17ada1cd32ceb
Leah <leah.miller@processmaker.com>
2018-07-24 15:12:24 -0700

    notation on what test need to include

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -19,8 +19,6 @@ class ApiTest extends TestCase
         $response->assetJsonStructure();
 
         // Check the JSON format
-
-        
     }
 
     public function testGetSingleItem()
@@ -36,17 +34,35 @@ class ApiTest extends TestCase
 
     }
 
-    public function testUpdate()
+    public function testUpdateSingleItem()
     {
+        $item_id = factory(App\Item::class)->make()->id;
+
+        $response = $this->get('/api/items/'.$item_id);
+
+        $response = assertViewHas($key)
+        $response->('name')
 
         $response = $this->put('/api/items/'.$item_id);
+        //create, update, check to see if it matches API
 
-        $response->assertStatus(200);
+        $response->assertStatus(200); //what status code should be placed here?
 
-        $response->assetJsonStructure();
+        $response->assetJsonStructure(); // why not assertJson
     }
 
-    public function testDeleteItem()
+    public function testPostNewItem()  //  create new entry in DB, place all data in necessary locations,make axios call to verify data is there
+    {
+        $item_id = factory(App\Item::class)->make()->id;
+
+        //what  are you posting? 
+        $this/*$item_name*/->assertEquals('', $item->name());  //if posting to 'name' the does the factory need to include name?
+
+        $response = $this->post('/api/items/'.$item_id);
+
+    }
+
+    public function testDeleteItem()  // axios call to see if data exists using item_id, remove data from DB using item_id, 
     {
 
         $item_id = factory(App\Item::class)->make()->id;
