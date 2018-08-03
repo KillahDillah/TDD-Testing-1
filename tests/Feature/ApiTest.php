31e8822c1a756712d09414e4f53893ac5a8288d8
Leah <leah.miller@processmaker.com>
2018-08-03 00:06:07 -0700

    updateSingleItem error invalid JSON returned from route

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -36,23 +36,20 @@ class ApiTest extends TestCase
 
     public function testUpdateSingleItem()
     {
-        $item = factory(\App\Item::class)->make([
-            'name' => 'Clean',
-            'id' => '1234',
-        ]);    //create new data, bring in data I specify
+        $item_id = factory(\App\Item::class)->make()->id;    //create new data, bring in data I specify
         
-        $response = $this->get('/api/items/'.$item->id);
+        $response = $this->get('/api/items/'.$item_id);
 
         $response->assertStatus(200);
         // $response = assertSee($item); // how to test if item is being returned?
         //should it be assertViewHas or some other variation to see if 'name' field is returned 
-        $response = $this->get('/api/items/'.$item->id);
+        $response = $this->get('/api/items/'.$item_id);
 
-        $response = $this->put('/api/items/'.$item->id, [
+        $response = $this->put('/api/items/'.$item_id, [
             'name' => 'Shop',
         ]);
 
-        $response = $this->get('/api/items/'.$item->id);
+        $response = $this->get('/api/items/'.$item_id);
         
         // $response->assertStatus(200); //what status code should be placed here? not always a 200
 
