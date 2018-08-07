4341e81ca06036cb35f7234da361dc3013222d5f
Leah <leah.miller@processmaker.com>
2018-08-07 10:14:09 -0700

    removed external vue to remove error messages in console.log

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,9 +67,7 @@
         <item-list></item-list>
         </div>   
 
-        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.15.3/axios.js"></script> -->
         <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-        <!-- <script src="https://unpkg.com/vue@2.5.16/dist/vue.js"></script> -->
         <script src="/js/app.js"></script>  
     </body>
 </html>
